f719904b7f2656b9263daaa7db3a92784ac35306
Alan Weik <alaan.m@hotmail.com>
2021-03-24 18:08:44 +0100

    Added two new razor pages, "JoinEvent" and "MyEvent".

---
+++ ASP.NET-Inlämningsuppgift1/Pages/MyEvents.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ASP.NET_Inlämningsuppgift1.Data;
+using ASP.NET_Inlämningsuppgift1.Models;
+
+namespace ASP.NET_Inlämningsuppgift1.Pages
+{
+    public class MyEventsModel : PageModel
+    {
+        private readonly ASP.NET_Inlämningsuppgift1.Data.EventDbContext _context;
+
+        public MyEventsModel(ASP.NET_Inlämningsuppgift1.Data.EventDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Event> Event { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Event = await _context.Events.ToListAsync();
+        }
+    }
+}
